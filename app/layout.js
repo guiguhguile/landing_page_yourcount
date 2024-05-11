8b7055b8dc31db7152f095c9caf672a7ad97c7e1
guiguhguile <130392565+guiguhguile@users.noreply.github.com>
2024-05-10 20:37:39 -0400

    add microsoft clarity integration

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter, Montserrat } from "next/font/google";
 import "./globals.css";
+import MicrosoftClarity from "./metrics/microsoft-clarity";
 
 const montserrat = Montserrat({ subsets: ["latin"], weight: ["400", "600"] });
 
@@ -10,11 +11,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className=" scroll-smooth">
+    <html lang="en" className="scroll-smooth">
       <body
         className={`${montserrat.className} scrollbar scrollbar-thumb-secundary-blue`}
       >
-        <main className="relative overflow-hidden">{children}</main>
+        <main className="relative overflow-hidden">
+          {children}
+          <MicrosoftClarity />
+        </main>
       </body>
     </html>
   );
